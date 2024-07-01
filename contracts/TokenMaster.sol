ca7f2ee1e123e3bfccfaf19ed12fd8aab0e82899
Petrus Breedt <petrus@global12.com>
2024-07-01 14:57:31 -0500

    Changes up to listing, before minting

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -1,4 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract TokenMaster {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TokenMaster is ERC721 {
+    address public owner;
+    uint256 public totalOccasions;
+
+    mapping(uint256 => Occasion) occasions;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) external onlyOwner {
+        totalOccasions++;
+        
+        occasions[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        );
+    }
+
+
+    function getOccasion(uint256 _id)
+    public view
+    returns (Occasion memory) {
+        return occasions[_id];
+    }
+
+}
--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,83 @@
 const { expect } = require("chai")
 
-describe("TokenMaster", () => {
+const NAME = "TokenMaster"
+const SYMBOL = "TM"
 
+const OCCASION_NAME = "ETH Texas"
+const OCCASION_COST = ethers.utils.parseUnits('1', 'ether')
+const OCCASION_MAX_TICKETS = 100
+const OCCASION_DATE = "Apr 27"
+const OCCASION_TIME = "10:00AM CST"
+const OCCASION_LOCATION = "Austin, Texas"
+
+describe("TokenMaster", () => {
+    let tokenMaster
+    let deployer, buyer
+  
+    beforeEach(async () => {
+      // Setup accounts
+      [deployer, buyer] = await ethers.getSigners()
+  
+      // Deploy contract
+      const TokenMaster = await ethers.getContractFactory(NAME)
+      tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+  
+      const transaction = await tokenMaster.connect(deployer).list(
+        OCCASION_NAME,
+        OCCASION_COST,
+        OCCASION_MAX_TICKETS,
+        OCCASION_DATE,
+        OCCASION_TIME,
+        OCCASION_LOCATION
+      )
+  
+      await transaction.wait()
+    })
+  
+    describe("Deployment", () => {
+      it("Sets the name", async () => {
+        expect(await tokenMaster.name()).to.equal(NAME)
+      })
+  
+      it("Sets the symbol", async () => {
+        expect(await tokenMaster.symbol()).to.equal(SYMBOL)
+      })
+  
+      it("Sets the owner", async () => {
+        expect(await tokenMaster.owner()).to.equal(deployer.address)
+      })
+    })
+
+    describe("Occasions", () => {
+        it('Returns occasions attributes', async () => {
+          const occasion = await tokenMaster.getOccasion(1)
+          expect(occasion.id).to.be.equal(1)
+          expect(occasion.name).to.be.equal(OCCASION_NAME)
+          expect(occasion.cost).to.be.equal(OCCASION_COST)
+          expect(occasion.tickets).to.be.equal(OCCASION_MAX_TICKETS)
+          expect(occasion.date).to.be.equal(OCCASION_DATE)
+          expect(occasion.time).to.be.equal(OCCASION_TIME)
+          expect(occasion.location).to.be.equal(OCCASION_LOCATION)
+        })
+    
+        it('Updates occasions count', async () => {
+          const totalOccasions = await tokenMaster.totalOccasions()
+          expect(totalOccasions).to.be.equal(1)
+        })
+      })
+
+      describe("Listing", () => {
+        it('Only owner can list occasions', async () => {
+          await expect(tokenMaster.connect(buyer).list(
+            OCCASION_NAME,
+            OCCASION_COST,
+            OCCASION_MAX_TICKETS,
+            OCCASION_DATE,
+            OCCASION_TIME,
+            OCCASION_LOCATION
+          )).to.be.reverted
+        })
+      })
+
+      
 })
